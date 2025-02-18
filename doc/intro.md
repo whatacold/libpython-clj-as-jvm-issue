5640e269bda1e06e9125928f10db888ce6179b0d
Ken Huang <whatacold@gmail.com>
2025-02-18 20:54:10 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to libpython-clj-as-jvm-issue
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/libpython_clj_as_jvm_issue/core.clj
@@ -0,0 +1,7 @@
+(ns libpython-clj-as-jvm-issue.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
